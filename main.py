7c463b075824d7ae1cc7f43a29bcf15ed60c5a58
vicayo <vicayo.c@gmail.com>
2025-07-01 20:44:09 -0400

    rebuild structure

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ from time import sleep as thread_sleep
 __chrome_driver_filepath: str = "./chromedriver/chromedriver_132_win64_x64.exe"
 
 
-workbook_filepath = "dx.xlsx"
+workbook_filepath = "./collected_data.xlsx"
 # workbook_filepath = ask_for_excel_filepath("Please enter the excel file path: ")
 workbook: ExcelWorkbook = load_excel_workbook(workbook_filepath)
 
-automation_for_sheet_1(
-    workbook["Sheet1"],
-    access_carrier_side=False
-)
+# automation_for_sheet_1(
+#     workbook["Sheet1"],
+#     access_carrier_side=False
+# )
 workbook.save(filename=workbook_filepath)
 print("\nSuccessfully saved workbook to disk.\n", end="")
