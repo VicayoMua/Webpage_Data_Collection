714754b05c7905ff3e16fc218df3f4eaa15d210a
vicayo <vicayo.c@gmail.com>
2025-07-01 23:07:58 -0400

    rebuild structure

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
+# from time import sleep as thread_sleep
+
 from ChromePageRender import (
     # something else
     Options as ChromeOptions,
     ChromePageRender
 )
 
+from dominate import document as HTMLDocument, tags as HTMLTags, util as HTMLUtils
+from bs4 import BeautifulSoup
+
 # '''
 #     You must properly set up this <__chrome_driver_filepath> to run this script..!
 # '''
 __chrome_driver_filepath: str = "./chromedrivers/chromedriver-win64-v138.0.7204.92/chromedriver.exe"
 
-from dominate import document as HTMLDocument
-from dominate.tags import h1, p, a
-from dominate.util import raw
-
-# from bs4 import BeautifulSoup
-
-# from time import sleep as thread_sleep
-
 new_document: HTMLDocument = HTMLDocument(title="国内外知名智库精选数据")
 
+
 def handler1(site_name: str, html_content: str):
+    # add site_name and html_content into new_document in an elegant way
     pass
+
 
 URLData = {
     '中国人民大学国家发展与战略研究院': {
@@ -51,7 +51,7 @@ for url_name in URLData.keys():
     url_data = URLData[url_name]
     (html_content, is_timed_out) = chrome_page_render.get_html_text_awaiting_selector(
         url=url_data['URL'],
-        selector_type=url_data['SelectorType'],
+        selector_type='css' if url_data['SelectorType'] == 'css' else 'xpath',
         selector_rules=url_data['SelectorRules'],
         timeout_seconds=30,
         print_error_log_to_console=True
@@ -63,4 +63,6 @@ for url_name in URLData.keys():
         url_name=url_name,
         html_content=html_content
     )
-
+with open("./generated_html/index.html", "w", encoding="utf-8") as f:
+    f.write(new_document.render(pretty=True))
+print("combined.html has been generated!")
